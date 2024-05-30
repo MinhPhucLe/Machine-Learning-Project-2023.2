65531bc3360f827157663bb0b190db5235c9a0c7
Nguyen Duc Thang <103989353+GeniusVenus@users.noreply.github.com>
2024-05-31 00:32:25 +0700

    New processed data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -88,7 +88,7 @@ def process_age(df) -> pd.DataFrame:
 
 # Convert DataFrane to csv file
 def insert_data(df):
-    df.to_csv('data/processed_data_1.csv', index = False)
+    df.to_csv('data/processed_data_phuc.csv', index = False)
 
 
 if __name__ == '__main__':
